22e95598d03bc8379d958994cb8b3c1cc3212898
Mark Guiang <mahikgot@gmail.com>
2026-01-10 21:14:09 +0800

    init commit

---
+++ src/main/java/dev/markguiang/oauth_meal/OauthMealApplication.java
@@ -0,0 +1,16 @@
+package dev.markguiang.oauth_meal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+
+public class OauthMealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthMealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/markguiang/oauth_meal/OauthMealApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.markguiang.oauth_meal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthMealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
