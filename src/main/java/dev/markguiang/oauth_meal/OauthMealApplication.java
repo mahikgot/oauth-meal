57d7be09581d4337937c8405e7d809ce3b496ba2
Mark Guiang <mahikgot@gmail.com>
2026-01-11 00:00:17 +0800

    chore: connect postgres

--- src/main/java/dev/markguiang/oauth_meal/OauthMealApplication.java
+++ src/main/java/dev/markguiang/oauth_meal/OauthMealApplication.java
@@ -17,16 +17,10 @@ package dev.markguiang.oauth_meal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(
-        exclude = {
-            DataSourceAutoConfiguration.class,
-            SecurityAutoConfiguration.class,
-            UserDetailsServiceAutoConfiguration.class
-        })
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class OauthMealApplication {
 
     public static void main(String[] args) {
