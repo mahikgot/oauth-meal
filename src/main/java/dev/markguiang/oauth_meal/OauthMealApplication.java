7886ba617f13c33bb8827c6a056523991596078e
Mark Guiang <mahikgot@gmail.com>
2026-01-10 22:44:39 +0800

    chore: remove security autoconfig

--- src/main/java/dev/markguiang/oauth_meal/OauthMealApplication.java
+++ src/main/java/dev/markguiang/oauth_meal/OauthMealApplication.java
@@ -18,8 +18,15 @@ package dev.markguiang.oauth_meal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(
+        exclude = {
+            DataSourceAutoConfiguration.class,
+            SecurityAutoConfiguration.class,
+            UserDetailsServiceAutoConfiguration.class
+        })
 public class OauthMealApplication {
 
     public static void main(String[] args) {
